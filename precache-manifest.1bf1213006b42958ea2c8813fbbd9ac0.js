99a7c05086c32f10c8432f5a63806d5c4b804590
Maluzzz <maaluuz@gmail.com>
2020-10-28 21:33:39 +0100

    Updates

---
+++ precache-manifest.1bf1213006b42958ea2c8813fbbd9ac0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da2be107eba9a8dfa74d1e65bddfbf42",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f0b9e5c010a372ac6dcb",
+    "url": "./static/css/main.1b9bcb54.chunk.css"
+  },
+  {
+    "revision": "d3fa8d8c01366f34cfc2",
+    "url": "./static/js/2.7c9f14cc.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.7c9f14cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0b9e5c010a372ac6dcb",
+    "url": "./static/js/main.6b471ae9.chunk.js"
+  },
+  {
+    "revision": "8c8c23543d758a9c5f1a",
+    "url": "./static/js/runtime-main.56ed76f3.js"
+  }
+]);
